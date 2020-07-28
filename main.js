78bcf9de75945758a75eabd499aec17eaa4b0f39
Autumn Fotopoulos <acarte94@gmail.com>
2020-07-27 19:01:42 -0500

    worked on cb,elementById, Query, Eventlistener

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log("main.js, hey!")
+"use strict"
+//  1. CALL BACKS
+// function yellGoodbyeToUser(user) {
+//     return `Goodbye ${user}!`;
+// }
+
+// function yellWhatsupToUser(user){
+//     return `Whatsup ${user}!`;
+
+// }
+
+// function makeGreeting(user,cb){
+//     return cb(user);
+// }
+
+// makeGreeting('Autumn', yellGoodbyeToUser);
+// // makeGreeting('Autumn', yellWhatsupToUser);
+
+// // console.log(makeGreeting('Autumn',yellWhatsupToUser))
+// console.log(makeGreeting('Autumn',yellGoodbyeToUser))
+// --------------------------------------------------------------------
+// 2.getElementById
+// const myBtn = document.getElementById('main-btn')
+// console.log(myBtn)
+
+// -----------------------------------------------------------------
+
+// 3.Query Seclector
+// if you use this to a class itll only select the first object
+
+// look on video accidentally deleted
+
+// // 4. Event- Listener
+// const otherBtn = document.querySelector(`#main-btn`)
+
+// const youClickedMe = () => {
+//     console.log("You clicked me!")
+// }
+
+// otherBtn.addEventListener('click',youClickedMe)
+
+
+// 5.Bootstrap
